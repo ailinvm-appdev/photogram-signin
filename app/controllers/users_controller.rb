c7590ef54d421078fe379cc05e08a0233bf2354d
ailinvm <ailinvm0@chicagobooth.edu>
2022-08-02 01:26:21 +0000

    0 percent but sign in and sing out works lol

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,41 @@
 class UsersController < ApplicationController
+  def authenticate
+    #get the username from params
+    un = params.fetch("input_username")
+    pw = params.fetch("input_password")
+    #get the password from params
+
+    # look up the record from the db matching username
+    user = User.where({ :username => un }).at(0)
+    # if there's no record, redirect back to sign in form
+    if user == nil
+      redirect_to("/user_sign_in", { :alert => "No one by that name here" })
+    else
+      # if there is a record, check the password matches
+      if user.authenticate (pw)
+        session.store(:user_id, user.id)
+        redirect_to("/", { :notice => "Welcome back!" })
+        # if not, rederict back to sign in form
+      else
+        redirect_to("/user_sign_in", { :alert => "bad pword" })
+        # if so, set the cookie
+      end
+    end
+  end
+
+  def new_session_form
+    render ({ :template => "users/signin_form.html.erb" })
+  end
+
+  def toast_cookies
+    reset_session
+    redirect_to("/", { :notice => "see ya later!" })
+  end
+
+  def new_registration_form
+    render ({ :template => "users/signup_form.html.erb" })
+  end
+
   def index
     @users = User.all.order({ :username => :asc })
 
@@ -16,21 +53,28 @@ class UsersController < ApplicationController
     user = User.new
 
     user.username = params.fetch("input_username")
+    user.password = params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_password_confirmation")
 
-    user.save
+    save_status = user.save
 
-    redirect_to("/users/#{user.username}")
+    if save_status == true
+      session.store(:user_id, user.id)
+
+      redirect_to("/users/#{user.username}", { :notice => "Welcome, " + user.username + "!" })
+    else
+      redirect_to("/user_sign_up", { :alert => user.errors.full_messages.to_sentence })
+    end
   end
 
   def update
     the_id = params.fetch("the_user_id")
     user = User.where({ :id => the_id }).at(0)
 
-
     user.username = params.fetch("input_username")
 
     user.save
-    
+
     redirect_to("/users/#{user.username}")
   end
 
@@ -42,5 +86,4 @@ class UsersController < ApplicationController
 
     redirect_to("/users")
   end
-
 end
